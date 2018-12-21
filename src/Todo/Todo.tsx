722e053a504160c6e059d56080fa29d02fc860a5
Mark Robson <mrobson@theworkshop.com>
2018-12-21 12:11:04 +0000

    refactor: adding mobx to todo

--- src/Todo/Todo.tsx
+++ src/Todo/Todo.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
 import './Todo.scss'
 
+@observer
 class Todo extends React.Component {
+
+  id = Math.random()
+  @observable title = ''
+
+  handleChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    this.title = ((event.target) as any).value
+  }
+
   public render() {
     return (
       <div className="Todo">
         <header className="Todo-header">
-          <h1 className="Todo-title">Welcome to React</h1>
+          <h1 className="Todo-title">{this.title}</h1>
         </header>
         <p className="Todo-intro">
-          To get started, edit <code>src/Todo.tsx</code> and save to reload.
+          <input value={this.title} onChange={this.handleChange} />
         </p>
       </div>
     )
